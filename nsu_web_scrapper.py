d31229e0e8778877f9cee5d495e94fbe7f977d5d
rifatulramim <99432484+rifatulramim@users.noreply.github.com>
2023-03-28 16:23:53 +0600

    Update nsu_web_scrapper.py
    get all links from nsu website

--- nsu_web_scrapper.py
+++ nsu_web_scrapper.py
@@ -16,11 +16,15 @@ for link in nsu.find_all('a'):
   if 'href' in link.attrs:
     links.append((link.attrs['href']))
 
+
+### Editing Dataframes of list
 # Converting the list to dataframe
 links_df = pd.DataFrame(links)
+print(links_df.head(2))
 
 # Naming the column
 links_df.columns = ['links']
+print(links_df.head(2))
 
 # Removing empty links from the dataframe
 links_df = links_df[links_df["links"].str.contains("#") == False]
@@ -42,10 +46,71 @@ print("External Links")
 print(external_links.head(2))
 
 
+### Editing the lists of links
+# Internal link and External Link seperated into their own list
+internal_link_list = []
+external_link_list = []
 
+for item in links:
+  if "http" in item:
+    external_link_list.append(item)
+  elif "#" in item:
+    continue
+  elif "pdf" in item:
+    continue
+  else:
+    internal_link_list.append(item)
+
+# Adding Http to intenal links 
+internal_link_list_full = []
+for item in internal_link_list:
+  temp = "http://www.northsouth.edu/" + item
+  internal_link_list_full.append(temp)
+
+print(len(internal_link_list_full))
+print(internal_link_list_full)
+
+# Getting links from the links in internal links list
+for item in internal_link_list_full:
+  print("Accessing URL -------------------------- " + item)
+  read_url = urlopen(item)
+  nsu_data = soup(read_url.read())
+  for link in nsu_data.find_all('a'):
+    if 'href' in link.attrs:
+      temp = link.attrs['href']
+      print("Link Found - " + temp)
+      if "http" in temp:
+        continue
+      elif "#" in temp:
+        continue
+      elif "pdf" in temp:
+        continue
+      elif "//" in temp:
+        continue
+      elif "gmail" in temp:
+        continue
+      elif ".com" in temp:
+        continue
+      elif temp in internal_link_list_full:
+        continue
+      else:
+        temp = "http://www.northsouth.edu/" + temp
+        print("Appending Link ----- " + temp)
+        internal_link_list_full.append(temp)
+
+print(len(internal_link_list_full))
+
+
+
+### For getting the data in the list
 for data in nsu(['style', 'script', 'a']):
   # Remove tags
   data.decompose()
 
 data = nsu.get_text()
 data.replace("\n", " ")
+
+website_data = pd.DataFrame({'address': ['http://www.northsouth.edu/'],
+                             'data': [data]})
+#website_data[data].replace("\n"," ")
+print(website_data)
